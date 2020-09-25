9c08634d888f0db8a9ef17a984aeeb9764d35629
ilina_al <aloschasd@yandex.ru>
2020-09-25 17:12:08 +0400

    git commit

--- hello_word.cpp
+++ hello_word.cpp
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
-int main(int argc, char **argv)
+#include <string.h>
+int main()
 {
-	printf("Hallo world!\n");
+	char name[255];
+	printf("Enter your name: ");
+	fgets(name, 255, stdin);
+	name[strlen(name)-1]='\0'; /* remove the newline at the end */
+	printf("Hallo %s!\n", name);
   return 0;
 }
