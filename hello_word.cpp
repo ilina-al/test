6b2f7ee131ff5835c7096766d3eb5386dac96ccb
ilina-al <71878195+ilina-al@users.noreply.github.com>
2020-09-25 19:02:43 +0400

    Update hello_word.cpp

--- hello_word.cpp
+++ hello_word.cpp
@@ -3,7 +3,7 @@
 int main()
 {
     char name[255];
-    printf("Enter your name: ");
+    printf("Enter your name: "); /* */
     fgets(name, 255, stdin);
     name[strlen(name) - 1] = '\0'; /* remove the newline at the end */
     printf("Hallo %s!\n", name);
